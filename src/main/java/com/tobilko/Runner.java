76d2099d2dbff224bcceb6554017409b50996dd0
Andrew Tobilko <andrew.tobilko@gmail.com>
2017-03-05 17:43:01 +0200

    i

---
+++ src/main/java/com/tobilko/Runner.java
@@ -0,0 +1,16 @@
+package com.tobilko;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by Andrew Tobilko on 05.03.17.
+ */
+@SpringBootApplication
+public class Runner {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Runner.class, args);
+    }
+
+}
